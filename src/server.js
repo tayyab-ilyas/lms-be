6501766615dee568cd77ae1aecad2eec000a367c
tayyab-ilyas <tayyabilyas963@gmail.com>
2025-09-15 00:18:10 +0530

    server conf

---
+++ src/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const app = require('./app');
+const connectDB = require('./config/database');
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+    console.error('Unhandled Rejection:', err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err.message);
+    process.exit(1);
+});
